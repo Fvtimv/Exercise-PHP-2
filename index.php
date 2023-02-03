bf1bd54b21c9d17d74a0c8a7aa07f547f1f2258c
Fvtimv <fatima.hilmi@hotmail.com>
2023-02-03 16:05:45 +0100

    Exercise 12 Complete with a var_dump and foreach

--- index.php
+++ index.php
@@ -382,3 +382,25 @@ function formaterDateFr($date){
 formaterDateFr("2018-02-23"); // ou formaterDateFr($date)
 
 ?>
+
+
+<h1>Exercice 12</h1>
+
+<p>La fonction var_dump($variable) permet d’afficher les informations 
+    d’une variable. Soit le tableau suivant :
+    $tableauValeurs=array(true,"texte",10,25.369,array("valeur1","valeur2"));
+    A l’aide d’une boucle, afficher les informations des variables contenues 
+    dans le tableau.</p>
+
+<?php
+
+$tableauValeurs = array (true, "texte", 10, 25.369, 
+                array ("valeur1","valeur2"));
+
+    foreach ($tableauValeurs as $value) {
+        echo var_dump($value) ;
+        echo "<br>";
+    }
+
+?>
+
